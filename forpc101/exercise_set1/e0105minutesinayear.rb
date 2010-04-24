5a5e5b71ea05a98d0a58120571b249e8b202e9a6
Michael Kohl <citizen428@gmail.com>
2010-04-24 15:40:17 +0200

    Removing old files

--- forpc101/exercise_set1/e0105minutesinayear.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-=begin
-  Calculates the minutes in a year
-  60 minutes/hour - 24 hours/day - 365 days/year
-=end
-MinutesInYear = 60 * 24 * 365
-
-puts "There are #{MinutesInYear} minutes in a year"
-
--- forpc101/exercise_set2/e0202convert.rb
+++
@@ -1,7 +0,0 @@
-=begin
-  Function to convert temperatures from Fahreheit to Celsius.
-  Dies if temp_in_fahrenheit is not a number.
-=end
-def convert(temp_in_fahrenheit)
-  (temp_in_fahrenheit - 32) * 5 / 9
-end
